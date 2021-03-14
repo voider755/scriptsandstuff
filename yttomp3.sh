5d81b6e319710cefb82c2ba3c9060d2685ff7336
voider755 <76598729+voider755@users.noreply.github.com>
2021-03-14 10:46:56 +0100

    Update yttomp3.sh
    Added comments

--- yttomp3.sh
+++ yttomp3.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+# Please see the README file. 
+# You need youtube-dl and ffmpeg. In a nutshell:
+# Paste a YT link and the script will:
+# 1. Download an audio file (or files, if a playlist) (*.m4a)
+# 2. Convert the *.m4a file to *.mp3
+# 3. Delete the *m4a file(s)
+# (c) voider755. MIT License
 read -p "Enter YT link: " ytlink
 youtube-dl -f 140 "$ytlink" &&
 for a in *.m4a
